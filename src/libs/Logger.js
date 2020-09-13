44b6ccd5a4277bd4a4b7ca3f37a9e5e3a3233b5b
Juan Panasiti <juanpanasiti@gmail.com>
2020-09-12 23:21:56 -0300

    Funciones de logs formateados para mejor lectura

---
+++ src/libs/Logger.js
@@ -0,0 +1,19 @@
+export const successLog = (msg) => {
+    console.log("\x1b[1m\x1b[5m\x1b[42m\x1b[30m%s\x1b[0m", "SUCCESS:");
+    console.log("\x1b[32m%s\x1b[0m", msg);
+}//successLog
+
+export const infoLog = (msg) => {
+    console.log("\x1b[1m\x1b[5m\x1b[46m\x1b[30m%s\x1b[0m", "INFO:");
+    console.log("\x1b[36m%s\x1b[0m", msg);
+}//infoLog
+
+export const warningLog = (msg) => {
+    console.log("\x1b[1m\x1b[5m\x1b[43m\x1b[30m%s\x1b[0m", "WARNING:");
+    console.log("\x1b[33m%s\x1b[0m", msg);
+}//warningLog
+
+export const errorLog = (msg) => {
+    console.log("\x1b[1m\x1b[5m\x1b[41m\x1b[37m%s\x1b[0m", "ERROR:");
+    console.log("\x1b[31m%s\x1b[0m", msg);
+}//errorLog
